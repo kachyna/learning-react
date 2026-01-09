236a1ee9f61438a0542f572fb962238361a24ecf
René Kachyňa <kachynarene@gmail.com>
2026-01-09 15:54:54 +0100

    feat: Create App componnent with comments

---
+++ learning-app/src/App.js
@@ -0,0 +1,14 @@
+// a component is a function that has the same name as the filename
+// a component must contain not-nulll return and an export line
+//      * return can return only one, single tag => all return markup should be wrapped in a container (like <div> or empty <>)
+// in fact, we are not writing HTML but JSX - javascript XML
+//      * it allows us to write HTML in javascript
+//      * anything in { } brackets is deemed as a javascript code
+
+const App = () => {
+    return (
+        <h1>Our first component.</h1>
+    )
+}
+
+export default App
