b63a81fe3b40439b2ce76c29e8b8f63767a82b4f
René Kachyňa <kachynarene@gmail.com>
2026-01-10 11:18:48 +0100

    feat: Add Task managet app

---
+++ to-do-app/src/App.js
@@ -0,0 +1,12 @@
+import AllTasks from "./components/AllTasks";
+
+function App() {
+  return (
+    <>
+      <h1>To-Do List</h1>
+      <AllTasks />
+    </>
+  );
+}
+
+export default App;
---
+++ to-do-app/src/components/AllTasks.js
@@ -0,0 +1,29 @@
+import "./AllTasks.css"
+import { useState } from "react"
+import data from "../data"
+
+const AllTasks = () => {
+    const [tasks, setTasks ] = useState( data )
+
+    const taskHandler = ( id ) => {
+        console.log("Clicked", id)
+        const filtered = tasks.filter( ( x ) => { return x.id !== id })
+        console.log( filtered )
+        setTasks( filtered )
+    }
+
+    return (
+        <div>
+            { tasks.map( (task) => { 
+                const {id, name} = task
+
+                return <div className="task" key={id}>
+                    <h4>{name}</h4>
+                    <button type="button" onClick={ () => taskHandler(id) }>Delete</button>
+                </div> 
+            })}
+        </div>
+    )
+}
+
+export default AllTasks
