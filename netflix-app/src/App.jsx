4aa20e2a2aef18ebde8e72a09d6c039bcebcc422
René Kachyňa <kachynarene@gmail.com>
2026-01-10 14:28:16 +0100

    feat: Add manage buttons to netflix-app

--- netflix-app/src/App.jsx
+++ netflix-app/src/App.jsx
@@ -10,11 +10,20 @@ function App() {
     setMovies( movies.filter( (movie) => { return movie.id != id } ))
   }
 
+  const deleteAllHandler = () => setMovies( [] )
+
+  const reloadAllHandler = () => setMovies( data )
+
   return ( 
   <>
     <h1>MyFlix Movie Library</h1>
      <div className="all-movies">
       { movies.map( ( movie ) => { return <MovieCard key={movie.id} movie={movie} onDelete={ () => deleteMovie( movie.id )}/> }) }
+    </div>
+    
+    <div className="bts-movies-managament">
+      <button type="button" onClick={ deleteAllHandler }>Delete all movies</button>
+      <button type="button" onClick={ reloadAllHandler }>Reload all movies</button>
     </div>
   </>
   )
