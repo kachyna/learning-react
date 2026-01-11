acf285f1bc1188d6e4205225b4faa09f854ad64f
René Kachyňa <kachynarene@gmail.com>
2026-01-11 10:35:10 +0100

    feat: Add category filters to netflix-app

--- netflix-app/src/App.jsx
+++ netflix-app/src/App.jsx
@@ -10,20 +10,25 @@ function App() {
     setMovies( movies.filter( (movie) => { return movie.id != id } ))
   }
 
-  const deleteAllHandler = () => setMovies( [] )
-
-  const reloadAllHandler = () => setMovies( data )
+  const filterMoviesByTag = ( tag ) => {
+    setMovies( data.filter( ( movie ) => { return movie.tags.includes( tag ) }))
+  }
 
   return ( 
   <>
     <h1>MyFlix Movie Library</h1>
+    <div className="filters">
+      <button type="button" onClick={ () => filterMoviesByTag("Vtipný") }>Comedy</button>
+      <button type="button" onClick={ () => filterMoviesByTag("Romantický") }>Romance</button>
+      <button type="button" onClick={ () => filterMoviesByTag("Gangster") }>Action</button>
+    </div>
      <div className="all-movies">
       { movies.map( ( movie ) => { return <MovieCard key={movie.id} movie={movie} onDelete={ () => deleteMovie( movie.id )}/> }) }
     </div>
     
     <div className="bts-movies-managament">
-      <button type="button" onClick={ deleteAllHandler }>Delete all movies</button>
-      <button type="button" onClick={ reloadAllHandler }>Reload all movies</button>
+      <button type="button" onClick={ () => setMovies( [] ) }>Delete all movies</button>
+      <button type="button" onClick={ () => setMovies( data ) }>Show all movies</button>
     </div>
   </>
   )
